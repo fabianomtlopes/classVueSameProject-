21b2fbaf00346ee793e9a70c9b94c95c1b915e98
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2023-03-20 09:55:13 -0300

    Projeto em andamento

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,22 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'Tarefas',
+    component: () => import('../views/Tarefas.vue'),
+
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/sobre',
+    name: 'Sobre',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () => import('../views/AboutView.vue'),
   },
 ];
 
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+
+         <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="campoInput"
+            label="Qual sua tarefa?"
+            outlined
+            clearable
+            @keyup.enter="handleAddTarefa"
+          ></v-text-field>
+        </v-col>
+
+      <v-list
+      flat
+      subheader
+    >
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div
+      v-for="tarefa, index in $store.state.tarefas"
+      :key="index"
+      >
+           <Tarefa
+           :tarefa="tarefa"
+           />
+      </div>
+
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+
+import Tarefa from '@/components/tarefas/tarefa.vue';
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Tarefa,
+  },
+  data() {
+    return {
+      campoInput: null,
+    };
+  },
+  methods: {
+    handleAddTarefa() {
+      this.$store.commit('adicionaTarefa', this.campoInput);
+      this.campoInput = null;
+    },
+  },
+});
+</script>
