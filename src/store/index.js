f9cd449491e32ed48d1ea6ea577891d480babce3
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2023-03-20 18:57:36 -0300

    Projeto sem typescript

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,51 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+import Localbase from 'localbase'
+
+const db = new Localbase('db')
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+      // { id: 1, titulo: 'Ir ao mercado', concluido: false },
+      // { id: 2, titulo: 'Comprar ração', concluido: false },
+    ],
   },
   getters: {
   },
+  // pode fazer algo com o state, editar
   mutations: {
+    buscaTarefas(state) {
+      db.collection('tarefas').get().then((tarefasDB) => {
+        state.tarefas = tarefasDB;
+      });
+    },
+
+    adicionaTarefa(state, titulo) {
+      db.collection('tarefas').add({
+        id: new Date().getTime(),
+        titulo,
+        concluido: false,
+      });
+    },
+
+    removeTarefa(state, id) {
+      state.tarefas = state.tarefas.filter((tarefa) => tarefa.id !== id);
+    },
+
+    editaTarefa(state, novaTarefa) {
+      const tarefa = state.tarefas.filter((tarefa) => tarefa.id === novaTarefa.id)[0];
+      tarefa.titulo = novaTarefa.titulo;
+    },
   },
+  // serve para otimizar o desenvolvimento
   actions: {
+    async adicionaTarefa({ commit }, titulo) {
+      await commit('adicionaTarefa', titulo);
+      await commit('buscaTarefas');
+    },
   },
   modules: {
-  }
+  },
 })
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+
+         <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="campoInput"
+            label="Qual sua tarefa?"
+            outlined
+            clearable
+            @keyup.enter="handleAddTarefa"
+          ></v-text-field>
+        </v-col>
+
+      <v-list
+      flat
+      subheader
+    >
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div
+      v-for="tarefa, index in $store.state.tarefas"
+      :key="index"
+      >
+           <Tarefa
+           :tarefa="tarefa"
+           />
+
+      </div>
+
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Tarefa from '../components/tarefas/Tarefa.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Tarefa,
+  },
+  data() {
+    return {
+      campoInput: null,
+    };
+  },
+
+  created() {
+    this.$store.commit('buscaTarefas');
+  },
+
+  methods: {
+    handleAddTarefa() {
+      // this.$store.commit('adicionaTarefa', this.campoInput);
+      this.$store.dispatch('adicionaTarefa', this.campoInput);
+      this.campoInput = null;
+    },
+  },
+});
+</script>
